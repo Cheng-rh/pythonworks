6f8d54e35a32d72c4f2bb9665f244b9914b42e99
chengruihui <chengruihui@nowledgedata.com.cn>
2018-09-27 09:20:34 +0800

    小视频转换为字符串

---
+++ pythonVideo/__init__.py
@@ -0,0 +1,12 @@
+# -!- coding: utf-8 -!-
+#!/usr/bin/python
+# Created with pycharm.
+# File Name: __init__.py 
+# User: bolao
+# Date: 2018/9/21 15:16
+# Version: V1.0
+# To change this template use File | Settings | File Templates.
+# Description:
+
+if __name__ == '__main__':
+    pass
